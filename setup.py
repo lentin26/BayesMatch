e150c225dbafb59088ea1019307b5c4bcd350237
lentin26 <miaroselentini@gmail.com>
2022-11-18 20:31:04 -0500

    added __init__.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='BayesMatch',
+    version='0.1.0',
+    description='Bayesian multi-view clustering for matching records of two '
+                'different type with share attributes (Maurya and Telang, 2017).',
+    packages=['BayesMatch'],
+    long_description=open('README.txt').read(),
+)
